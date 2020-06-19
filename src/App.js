6e7434644b671a2fd03bc86485b1f198dc6b824f
Maciej Kożuszek <mackozuszek@gmail.com>
2020-06-19 13:22:29 +0200

    Final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const useSetState = (initialState = {}) => {
+  const [state, set] = useState(initialState);
+  const setState = useCallback(
+    (patch) => {
+      set((prevState) => {
+        return Object.assign(
+          {},
+          prevState,
+          patch instanceof Function ? patch(prevState) : patch
+        );
+      });
+    },
+    [set]
   );
-}
+  return [state, setState];
+};
 
-export default App;
+export default useSetState;
