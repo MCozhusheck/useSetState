653d47e3355a755c15cda2737fa968c2afbeaa82
Maciej Kożuszek <mackozuszek@gmail.com>
2020-06-19 17:15:44 +0200

    Callback to setState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,73 @@
-import { useState, useCallback } from "react";
+import React, { useLayoutEffect, useState, useCallback, useRef } from "react";
+import "./App.css";
+
+function App() {
+  let [state, setState] = useSetState({ counter: 0 });
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          setState(
+            (prevState) => ({
+              counter: prevState.counter + 1,
+            }),
+            () => console.log(state)
+          );
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setState((prevState) => ({
+            counter: prevState.counter - 1,
+          }));
+        }}
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => setState({ counter: 100 }, () => console.log(state))}
+      >
+        set to 100
+      </button>
+      <p>{state.counter}</p>
+    </div>
+  );
+}
 
 const useSetState = (initialState = {}) => {
   const [state, set] = useState(initialState);
-  const setState = useCallback(
+  const updateState = useCallback(
     (patch) => {
       set((prevState) => {
-        return Object.assign(
-          {},
-          prevState,
-          patch instanceof Function ? patch(prevState) : patch
-        );
+        if (patch instanceof Function) {
+          return Object.assign({}, prevState, patch(prevState));
+        } else {
+          return Object.assign(
+            {},
+            prevState,
+            patch instanceof Function ? patch(prevState) : patch
+          );
+        }
       });
     },
     [set]
   );
+  let pendingCallbacks = useRef();
+  useLayoutEffect(() => {
+    if (pendingCallbacks.current instanceof Function) {
+      pendingCallbacks.current();
+      pendingCallbacks.current = {};
+    }
+  });
+  const setState = function (patch, cb) {
+    updateState(patch);
+    if (cb instanceof Function) {
+      pendingCallbacks.current = cb;
+    }
+  };
   return [state, setState];
 };
 
-export default useSetState;
+export default App;
